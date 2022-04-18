69dc1c82cff7379f00fea1dd077fd17ea966f471
Osvaldo Hernan Ceballos <hernan.casla@gmail.com>
2022-04-18 14:42:31 +0200

    init

--- server.js
+++ server.js
@@ -1,30 +1,17 @@
-var path = require('path'),
+/*var path = require('path'),
     fs = require("fs");
+const readline = require('readline');
 
-//joining path of directory
 const directoryPath = path.join(__dirname, 'responses');
 var responseFiles = [];
-//passsing directoryPath and callback function
-fs.readdir(directoryPath, function (err, files) {
-    if (err) {
-        return console.log('Unable to scan directory: ' + err);
-    }
-    files.forEach(function (file) {
-        // Do whatever you want to do with the file
-        console.log(file);
-        let key = file.replace(/\.[^/.]+$/, "");
-        responseFiles[key] = path.join(directoryPath,file);
-    });
-});
+
 async function processLineByLine() {
-    const fileStream = fs.createReadStream('datos_nomivac_covid19');
+    const fileStream = fs.createReadStream('resources/head-10.csv');
 
     const rl = readline.createInterface({
         input: fileStream,
         crlfDelay: Infinity
     });
-    // Note: we use the crlfDelay option to recognize all instances of CR LF
-    // ('\r\n') in input.txt as a single line break.
 
     for await (const line of rl) {
         // Each line in input.txt will be successively available here as `line`.
@@ -32,4 +19,19 @@ async function processLineByLine() {
     }
 }
 
-processLineByLine();
+processLineByLine();*/
+
+const fs = require('fs');
+const path = require('path');
+const csv = require('fast-csv');
+var i = 0;
+
+function processUnitaryData(data){
+    console.log(i++);
+}
+
+fs.createReadStream(path.resolve(__dirname, 'resources', 'head-10.csv'))
+    .pipe(csv.parse({ headers: true }))
+    .on('error', error => console.error(error))
+    .on('data', processUnitaryData)
+    .on('end', rowCount => console.log(`Parsed ${rowCount} rows`));
