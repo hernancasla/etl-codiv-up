5120553bd584583283501b00a405b630a5811094
Osvaldo Hernan Ceballos <hernan.casla@gmail.com>
2022-04-19 17:44:51 +0200

    init

--- server.js
+++ server.js
@@ -1,33 +1,35 @@
-/*var path = require('path'),
-    fs = require("fs");
-const readline = require('readline');
-
-const directoryPath = path.join(__dirname, 'responses');
-var responseFiles = [];
-
-async function processLineByLine() {
-    const fileStream = fs.createReadStream('resources/head-10.csv');
-
-    const rl = readline.createInterface({
-        input: fileStream,
-        crlfDelay: Infinity
-    });
-
-    for await (const line of rl) {
-        // Each line in input.txt will be successively available here as `line`.
-        console.log(`Line from file: ${line}`);
-    }
-}
-
-processLineByLine();*/
-
 const fs = require('fs');
 const path = require('path');
 const csv = require('fast-csv');
 var i = 0;
+var index = 0;
+const modValue = 11;
+const outputDirectory = path.resolve(__dirname, 'output');
+
+fs.readdir(outputDirectory, (err, files) => {
+    if (err) throw err;
+
+    for (const file of files) {
+        fs.unlink(path.join(outputDirectory, file), err => {
+            if (err) throw err;
+        });
+    }
+});
 
 function processUnitaryData(data){
-    console.log(i++);
+    let insert = "";
+    let separator = ","
+    if(i%modValue==modValue-1){
+        separator = ";"
+    }
+    if(i%modValue==0){
+        index = i/modValue;
+        insert = "insert into vactination(row1,row2,row3,row4,row5) values\n";
+    }
+    let sentence = `${insert}(\'${data.sexo}','${data.grupo_etario}\',\'${data.jurisdiccion_residencia_id}\',\'${data.depto_residencia}\',${data.fecha_aplicacion})${separator}\n`;
+    let filePath = path.resolve(__dirname, 'output', `example_file_${index}.txt`);
+    fs.appendFileSync(filePath, sentence, {encoding:"utf8",flag:"a"}, (err) => {if(err) console.log(err)});
+    i++;
 }
 
 fs.createReadStream(path.resolve(__dirname, 'resources', 'head-10.csv'))
